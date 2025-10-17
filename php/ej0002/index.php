7ec56003cae6a076cb7c040216ea89b0bb54ddd7
antoniora97 <antoniora1997@gmail.com>
2025-10-17 21:28:57 +0200

    Crea una función que reciba una frase y devuelva la palabra más larga dentro de ella con PHP

---
+++ php/ej0002/index.php
@@ -0,0 +1,22 @@
+<!-- 
+    Crea una función que reciba una frase y devuelva 
+    la palabra más larga dentro de ella. 
+-->
+
+<?php
+    function palabraMasLarga($texto) {
+        $frase = preg_replace("/[^\p{L}\s]/u", "", $frase);
+        $texto = strtolower($texto);
+        $palabras = explode(" ", $texto);
+        $palabraMasLarga = "";
+        foreach ($palabras as $palabra) {
+            if (strlen($palabra) > strlen($palabraMasLarga)) {
+                $palabraMasLarga = $palabra;
+            }
+        }
+        return $palabraMasLarga;
+    }
+
+    $texto = "¿Qué le dice el perro al camaleón? Ladrillo.";
+    echo "La palabra más larga es " . palabraMasLarga($texto);
+?>
