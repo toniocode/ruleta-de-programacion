d62cc463109ca7e6850b8600fa0e5beeb049f075
antoniora97 <antoniora1997@gmail.com>
2025-10-24 11:56:55 +0200

    Determina si una cadena de texto es un panagrama con JavaScript

---
+++ javascript/ej0004/main.js
@@ -0,0 +1,20 @@
+// Determina si una cadena de texto es un panagrama 
+// (contiene todas las letras del alfabeto al menos 
+// una vez)
+
+function esPanagrama(texto) {
+    texto = texto.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    const letras = new Set();
+    for (let letra of texto) {
+        if (letra >= 'a' && letra <= 'z') {
+            letras.add(letra);
+        }
+    }
+
+    return letras.size === 26;
+}
+
+texto = "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.";
+
+console.log(esPanagrama(texto))
