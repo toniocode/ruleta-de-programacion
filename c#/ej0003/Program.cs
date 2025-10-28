405513408d2e0daee2abec1cb26ad133a3b3266f
antoniora97 <antoniora1997@gmail.com>
2025-10-28 18:01:29 +0100

    Determina si un array contiene un número dominante con C#

---
+++ c#/ej0003/Program.cs
@@ -0,0 +1,36 @@
+﻿// Determina si en una lista de números existe un número 
+// dominante, es decir, un número que aparece más veces 
+// que la mitad del tamaño total de la lista.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr1 = [3, 3, 4, 2, 3, 3, 5];
+        int[] arr2 = [1, 2, 3, 3];
+
+        HasDominantNumber(arr1);
+        HasDominantNumber(arr2);
+    }
+    
+    static void HasDominantNumber(int[] arr)
+    {
+        Dictionary<int, int> counter = [];
+        foreach (int n in arr)
+        {
+            counter[n] = counter.TryGetValue(n, out int value)
+                ? ++value
+                : 1;
+
+            if (counter[n] > arr.Length / 2)
+            {
+                Console.WriteLine($"El número dominante es {n}.");
+                return;
+            }
+        }
+
+        Console.WriteLine("El array no tiene número dominante.");
+    }
+}
